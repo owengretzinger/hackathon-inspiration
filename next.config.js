b439aba614700a37b4d89a40eea3a94da931e32d
Owen Gretzinger <owengretzinger@gmail.com>
2025-02-03 15:11:16 -0500

    remove next config options for markdown

--- next.config.js
+++ next.config.js
@@ -15,25 +15,6 @@ const config = {
       },
     ],
   },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.md": {
-          loaders: ["raw-loader"],
-          as: "*.js",
-        },
-      },
-    },
-  },
-  webpack: (config) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    config.module.rules.push({
-      test: /\.md$/,
-      type: "asset/source",
-    });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return config;
-  },
 };
 
 export default config;
